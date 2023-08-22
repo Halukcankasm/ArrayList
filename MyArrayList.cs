c25945c8680713cceb6acc85e0623059e6f67f36
halukcan kasım <halukcankasim@hotmail.com>
2023-08-22 20:28:57 +0300

    Add project files.

---
+++ MyArrayList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    public class MyArrayList
+    {
+        private object[] myArray { get; set; }
+        private int index = 0;
+        public object this[int i]
+        {
+            get => myArray[i];
+            set => myArray[i] = value;
+        }
+        public MyArrayList()
+        {
+            myArray = new object[5];
+        }
+
+        public MyArrayList(int count)
+        {
+            myArray = new object[count];
+        }
+
+        public void Add(object value)
+        {
+            int newLenth = myArray.Length * 2;
+            CheckLengthOfArray();
+            myArray[index] = value;
+            index++;
+        }
+
+        public void CheckLengthOfArray()
+        {
+            object[] arraySize = myArray;
+            if (index == (myArray.Length - 1))
+            {
+                Array.Resize(ref arraySize, (myArray.Length * 2));
+            }
+            myArray = arraySize;
+        }
+
+        /// <summary>
+        /// Delete Last Item
+        /// </summary>
+        public void Remove()
+        {
+            var x = myArray.Last();
+            myArray = myArray.Where(a => a != x).ToArray();
+        }
+
+
+        /// <summary>
+        /// Delete with Index
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            myArray = myArray.Where(a => a != myArray[index]).ToArray();
+        }
+
+        /// <summary>
+        /// Delete with item
+        /// </summary>
+        /// <param name="index"></param>
+        public void Remove(object value)
+        {
+            myArray = myArray.Where(a => a != value).ToArray();
+        }
+
+
+        /// <summary>
+        /// Reverse ArrayList
+        /// </summary>
+        public void Reverse()
+        {
+            myArray.Reverse();
+        }
+
+
+        public int Count()
+        {
+            return myArray.Length;
+        }
+
+        public int GetIndexOfItem(object item)
+        {
+            int ind = 1;
+            for (int i = 0; i <= myArray.Length -1; i++)
+            {
+                if (myArray[i] == item)
+                {
+                    ind = i;
+                }
+            }
+            return ind;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.ArrayList lst = new System.Collections.ArrayList(capacity:25);
+            Console.WriteLine(lst.Capacity);
+
+            lst.Add(1);
+            lst.Add(34);
+            lst.Add(4.98F);
+            lst.Add(true);
+            lst.Add("Csharp");
+
+
+            Console.WriteLine("----------------MyArrayList--------------------");
+            MyArrayList myArrayList = new MyArrayList();
+            Console.WriteLine(myArrayList.Count());
+
+            myArrayList.Add(1);
+            Console.WriteLine(myArrayList[1]);
+            Console.WriteLine(myArrayList.Count());
+
+
+            myArrayList.Add("Example");
+            Console.WriteLine(myArrayList[2]);
+            Console.WriteLine(myArrayList.Count());
+
+
+            myArrayList.Add(2);
+            Console.WriteLine(myArrayList[3]);
+            Console.WriteLine(myArrayList.Count());
+
+
+            myArrayList.Add(3);
+            Console.WriteLine(myArrayList[4]);
+            Console.WriteLine(myArrayList.Count());
+
+
+            myArrayList.Add(4);
+            Console.WriteLine(myArrayList[5]);
+            Console.WriteLine(myArrayList.Count());
+
+
+            myArrayList.Add(5);
+            Console.WriteLine(myArrayList[6]);
+            Console.WriteLine(myArrayList.Count());
+
+
+            Console.WriteLine(myArrayList.Count());
+            myArrayList.Add(6);
+            Console.WriteLine(myArrayList[7]);
+
+
+
+            myArrayList.Add("Example");
+            Console.WriteLine(myArrayList.Count());
+        }
+    }
+}
